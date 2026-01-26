e881c4717671214d15f08e15c44f6850b9067eb5
lulula <ooo@bbb.com>
2026-01-26 22:08:15 +0800

    Refactor __init__.py to define mappings inline

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import VibeVoiceLoader, VibeVoiceTranscribe, VibeVoiceSaveFile
+
+NODE_CLASS_MAPPINGS = {
+    "VibeVoiceLoader": VibeVoiceLoader,
+    "VibeVoiceTranscribe": VibeVoiceTranscribe,
+    "VibeVoiceSaveFile": VibeVoiceSaveFile
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "VibeVoiceLoader": "VibeVoice Model Loader",
+    "VibeVoiceTranscribe": "VibeVoice Transcribe",
+    "VibeVoiceSaveFile": "VibeVoice Save File"
+}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
