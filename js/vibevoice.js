d6a1f6f49feeeaba637b2d05aa69cf7560cb4345
lulula <ooo@bbb.com>
2026-01-27 02:03:57 +0800

    Fix JS extension to reliably display text on VibeVoiceShowText node

--- js/vibevoice.js
+++ js/vibevoice.js
@@ -5,30 +5,33 @@ app.registerExtension({
     name: "VibeVoice.ShowString",
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
         if (nodeData.name === "VibeVoiceShowText") {
+
+            // 1. Ensure widget exists when node is created
+            const onNodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function () {
+                const r = onNodeCreated ? onNodeCreated.apply(this, arguments) : undefined;
+
+                // Check if widget already exists (from serialization)
+                if (!this.widgets || !this.widgets.find(w => w.name === "text")) {
+                    // Create a multiline string widget
+                    const w = ComfyWidgets["STRING"](this, "text", ["STRING", { multiline: true }], app).widget;
+                    w.inputEl.readOnly = true;
+                    w.inputEl.style.opacity = 0.6;
+                }
+                return r;
+            };
+
+            // 2. Update widget when execution completes
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (message) {
                 onExecuted?.apply(this, arguments);
 
-                // message.text is the list of strings returned by python {"ui": {"text": [...]}}
                 if (message && message.text) {
                     const text = message.text.join("");
-
-                    // Try to find existing widget to update
-                    let w = this.widgets?.find((w) => w.name === "text_output");
-
-                    if (!w) {
-                        // Create new widget if not exists
-                        // Note: ComfyWidgets["STRING"] creates a wrapper, accessing .widget gets the actual widget object
-                        // Parameters: node, inputName, [type, options], app
-                        const widgetWrapper = ComfyWidgets["STRING"](this, "text_output", ["STRING", { multiline: true }], app);
-                        w = widgetWrapper.widget;
-                        w.inputEl.readOnly = true;
-                        w.inputEl.style.opacity = 0.8;
-                    }
-
+                    const w = this.widgets?.find((w) => w.name === "text");
                     if (w) {
                         w.value = text;
-                        this.onResize?.(this.size); // Trigger resize to fit text
+                        this.onResize?.(this.size);
                     }
                 }
             };
